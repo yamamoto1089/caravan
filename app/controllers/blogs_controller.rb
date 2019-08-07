e0304a724ab00aa27a45e3ccaeba2c4b43e9121e
ymmt1089 <ymmt1089@gmail.com>
2019-08-07 19:33:01 +0900

    [update] 4-4

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,30 @@
+class BlogsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def index
+  	# 記事を全件取得
+  	@blogs = Blog.all
+  end
+
+  def new
+  	@blog = Blog.new
+  end
+
+  def create
+  	blog = Blog.new(blog_params)
+  	blog.save
+  	redirect_to blogs_path
+  end
+
+  def edit
+  end
+
+
+  def blog_params
+  	params.require(:blog).permit(:title,:category,:body)
+  end
+end
